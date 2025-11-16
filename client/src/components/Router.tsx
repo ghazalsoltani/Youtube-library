b501583bbc17c244f75b60ff37047855a811cb90
Ghazal <ghazal.soltaninasab@gmail.com>
2025-11-16 21:30:01 +0100

    Enhance: Add linting, formatting, and improved TypeScript config

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -1,12 +1,16 @@
 //importations
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Library from '../pages/Library';
 
 const Router: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
+                {/* Redirect root to /home */}
+                <Route path="/" element={<Navigate to="/home" replace />} />
+                
+                {/* Library page with username */}
                 <Route path="/:username" element={<Library />} />
             </Routes>
         </BrowserRouter>
